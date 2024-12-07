fa1d875447674daa701527e3646bff1c3a2b5113
aytsan-ns <nastyasmirnova40@gmail.com>
2024-12-07 23:08:52 +0300

    Методы с переменным числом параметров

---
+++ practica8.java
@@ -0,0 +1,28 @@
+public class practica8 {
+    static class Printer {
+        // Метод для печати целых чисел
+        public void printValues(int... numbers) {
+            for (int num : numbers) {
+                System.out.println(num);
+            }
+        }
+
+        // Метод для печати строк
+        public void printValues(String... strings) {
+            for (String str : strings) {
+                System.out.println(str);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+
+        // Вызов метода для целых чисел
+        printer.printValues(1, 2, 3, 4, 5);
+
+        // Вызов метода для строк
+        printer.printValues("Apple", "Banana", "Cherry");
+
+    }
+}
